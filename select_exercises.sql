e521ecdab1881bd10884600fb31dbe7b31a32fec
Isaac Bernal <isaacb1598@gmail.com>
2023-11-27 16:06:51 -0600

    completed select exercises

---
+++ select_exercises.sql
@@ -0,0 +1,11 @@
+SELECT * FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT * FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT * FROM albums WHERE genre = 'Soft rock';
+
+SELECT * FROM albums WHERE release_date = 1990;
+
+SELECT * FROM albums WHERE sales < 20;
+
+SELECT * FROM albums WHERE genre = 'Rock';
